df46e8e85043c28d22111738d304760b988a7dc1
Darin Kadrioski <dkadrios@gmail.com>
2018-10-11 21:09:31 -0700

    Giving the parent app more control over the look of the notepicker

--- src/pickers/NotePicker.js
+++ src/pickers/NotePicker.js
@@ -1,71 +1,24 @@
-// Adapted from https://material-ui-next.com/demos/autocomplete/
-
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Autosuggest from 'react-autosuggest'
 import match from 'autosuggest-highlight/match'
 import parse from 'autosuggest-highlight/parse'
-import TextField from '@material-ui/core/TextField'
-import Paper from '@material-ui/core/Paper'
-import MenuItem from '@material-ui/core/MenuItem'
-import { withStyles } from '@material-ui/core/styles'
 import { mappingShape } from '../shapes'
 import { assertRange } from '..'
 
 const renderedName = item => `${item.note} ${item.name.length ? '-' : ''} ${item.name}`
 
-function renderInput(inputProps) {
-  const { classes, disabled, autoFocus, value, ref, ...rest } = inputProps
-
-  return (
-    <TextField
-      autoFocus={autoFocus}
-      className={classes.textField}
-      disabled={disabled}
-      value={value}
-      inputRef={ref}
-      FormHelperTextProps={{ className: classes.whiteLabel }}
-      InputProps={{
-        ...rest,
-      }}
-    />
-  )
-}
-
-function renderSuggestion(suggestion, { query, isHighlighted }) {
+const renderSuggestion = (suggestion, { query }) => {
   const matches = match(suggestion, query)
   const parts = parse(suggestion, matches)
 
   return (
-    <MenuItem selected={isHighlighted} component="div">
-      <div>
-        {parts.map((part, index) => part.highlight ? (
-          <span key={String(index)} style={{ fontWeight: 300 }}>
-            {part.text}
-          </span>
-        ) : (
-          <strong key={String(index)} style={{ fontWeight: 500 }}>
-            {part.text}
-          </strong>
-        ))}
-      </div>
-    </MenuItem>
+    <Fragment>
+      {parts.map((part, index) => (
+        <span key={String(index)}>{part.text}</span>
+      ))}
+    </Fragment>
   )
-}
-
-function renderSuggestionsContainer(options) {
-  const { containerProps, children } = options
-
-  return (
-    <Paper {...containerProps} square>
-      {children}
-    </Paper>
-  )
-}
-
-function getSuggestionValue(suggestion) {
-  // this.props.onChange(suggestion)
-  return suggestion
 }
 
 function getSuggestions(source, value) {
@@ -77,36 +30,8 @@ function getSuggestions(source, value) {
     : source.filter(suggestion => suggestion.toLowerCase().includes(inputValue))
 }
 
-const styles = theme => ({
-  container: {
-    // position: 'relative',
-  },
-  suggestionsContainerOpen: {
-    position: 'absolute',
-    marginTop: theme.spacing.unit,
-    marginBottom: theme.spacing.unit * 3,
-    left: 0,
-    right: 0,
-    zIndex: 5,
-    maxHeight: 200,
-    overflowY: 'scroll',
-  },
-  suggestion: {
-    display: 'block',
-  },
-  suggestionsList: {
-    margin: 0,
-    padding: 0,
-    listStyleType: 'none',
-  },
-  textField: {
-    width: '100%',
-  },
-})
-
 class NotePicker extends React.Component {
   static propTypes = {
-    classes: PropTypes.object.isRequired,
     mapping: PropTypes.arrayOf(mappingShape).isRequired,
     onChange: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
@@ -171,30 +96,21 @@ class NotePicker extends React.Component {
   }
 
   render() {
-    const { classes, mapping, onChange, value: propValue, inputRef, ...rest } = this.props
+    const { mapping, onChange, value: propValue, inputRef, ...rest } = this.props
     const { suggestions, value } = this.state
 
     return (
       <Autosuggest
         ref={this.storeInputReference}
-        theme={{
-          container: classes.container,
-          suggestionsContainerOpen: classes.suggestionsContainerOpen,
-          suggestionsList: classes.suggestionsList,
-          suggestion: classes.suggestion,
-        }}
-        renderInputComponent={renderInput}
         suggestions={suggestions}
         shouldRenderSuggestions={() => true}
         onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
         onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
         onSuggestionSelected={this.handleSuggestionSelected}
-        renderSuggestionsContainer={renderSuggestionsContainer}
-        getSuggestionValue={getSuggestionValue}
+        getSuggestionValue={f => f}
         renderSuggestion={renderSuggestion}
         inputProps={{
-          classes,
-          placeholder: 'Note # or instrument name',
+          placeholder: 'Note # or name',
           value,
           onChange: this.handleChange,
           onFocus: ({ target }) => target.select(),
@@ -205,4 +121,4 @@ class NotePicker extends React.Component {
   }
 }
 
-export default withStyles(styles)(NotePicker)
+export default NotePicker
