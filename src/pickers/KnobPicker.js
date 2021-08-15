e42b75acfa4cd8763fbccbf2c8331ef481dbf5ed
Darin Kadrioski <dkadrios@gmail.com>
2021-08-14 18:27:55 -0700

    can include a dropdown with knob

--- src/pickers/KnobPicker.js
+++ src/pickers/KnobPicker.js
@@ -1,32 +1,59 @@
+import { forwardRef, useMemo } from 'react'
 import PropTypes from 'prop-types'
+import { arraySequence } from '../utils'
+import Select from './Select'
 import Knob from './Knob'
 
-const KnobPicker = (props) => {
-  const { label, shrinkLabel, ...rest } = props
-  return shrinkLabel
-    ? (
-      <div className="zds-pickers__container">
-        <div>
-          <Knob {...rest} />
-        </div>
+const KnobPicker = forwardRef((props, ref) => {
+  const {
+    disabled,
+    highToLow,
+    includePicker,
+    label,
+    max,
+    min,
+    shrinkLabel,
+    ...rest
+  } = props
+
+  const options = useMemo(
+    () => {
+      const result = arraySequence(max - min + 1)
+        .map(i => min + i)
+        .map(value => ({ value, label: value }))
+      return highToLow ? result.reverse() : result
+    },
+    [highToLow, max, min],
+  )
+
+  return (
+    <div className="zds-pickers__container">
+      {Boolean(!shrinkLabel && Boolean(label)) && <span className="zds-pickers__label">{label}</span>}
+      <div style={{ display: 'inline-flex', gap: 10 }}>
+        {Boolean(includePicker) && (
+          <Select
+            {...rest}
+            disabled={disabled}
+            isDisabled={disabled}
+            options={options}
+            ref={ref}
+          />
+        )}
+
+        <Knob {...{ max, min, ...rest }} />
       </div>
-    )
-    : (
-      <div className="zds-pickers__container">
-        {Boolean(label) && <span className="zds-pickers__label">{label}</span>}
-        <div>
-          <Knob {...rest} />
-        </div>
-      </div>
-    )
-}
+    </div>
+  )
+})
 
 KnobPicker.propTypes = {
   disabled: PropTypes.bool,
+  highToLow: PropTypes.bool,
+  includePicker: PropTypes.bool,
   label: PropTypes.string,
   max: PropTypes.number,
   min: PropTypes.number,
-  onChange: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
   shrinkLabel: PropTypes.bool,
   size: PropTypes.number,
   value: PropTypes.number,
@@ -35,10 +62,11 @@ KnobPicker.propTypes = {
 
 KnobPicker.defaultProps = {
   disabled: false,
+  highToLow: false,
+  includePicker: false,
   label: undefined,
   max: 127,
   min: 0,
-  onChange: undefined,
   shrinkLabel: false,
   size: 100,
   value: 0,
