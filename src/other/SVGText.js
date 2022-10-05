c9c8b78a700ad3b7a724aba9487f5efa7a85a46e
Darin Kadrioski <darin@nebiru.com>
2022-10-04 20:28:21 -0700

    package updates

--- src/other/SVGText.js
+++ src/other/SVGText.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { numberOrString } from '../shapes'
