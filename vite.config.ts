a9f7d2aef9176a605ee8c49157613516f499afae
Darin Kadrioski <dkadrios@icloud.com>
2025-01-17 20:24:16 -0800

    preparing to publish

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,31 @@ import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [
+    react(),
+    dts({
+      include: ['lib'],
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es'],
+      name: 'zds-pickers',
+      formats: ['es', 'cjs'],
+      fileName: format => `index.${format}.js`,
     },
+    sourcemap: true,
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    minify: 'esbuild',
   },
 })
